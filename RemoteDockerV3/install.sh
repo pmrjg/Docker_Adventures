dcba7d3b81e37dc63996747d19fbd15eb80ff1ff
Pedro Jorge <pmrjge@gmail.com>
2025-08-16 16:59:05 +0100

    V3 ready for experimenting

--- RemoteDockerV3/install.sh
+++ RemoteDockerV3/install.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-conda install -n gen nvidia/label/cuda-13.0.0::cuda-toolkit nvidia/label/cudnn-9.9.0::cudnn -y
+conda install -n gen nvidia/label/cuda-13.0.0::cuda-compiler nvidia/label/cuda-13.0.0::cuda-toolkit nvidia/label/cudnn-9.12.0::cudnn
