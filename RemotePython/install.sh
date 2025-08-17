a723164a995cce164e5f3c85bfc97aefb54b10cd
Pedro Jorge <pmrjge@gmail.com>
2025-08-17 16:05:35 +0100

    Final remote pyenv

--- RemotePython/install.sh
+++ RemotePython/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 conda run -n pt pip install --no-cache-dir datasets peft transformers bitsandbytes
-conda run -n pt pip install --no-cache-dir langchain langchain-huggingface langchain-nvidia langchain-openai langchain-chroma
+conda run -n pt pip install --no-cache-dir langchain langchain-huggingface langchain-openai langchain-chroma chromadb
 conda run -n pt pip install --no-cache-dir jupyterlab notebook ipywidgets matplotlib pandas sympy scipy tqdm
 conda run -n pt pip install --no-cache-dir accelerate triton
 conda run -n pt pip install --no-cache-dir torch torchvision --index-url https://download.pytorch.org/whl/cu129
